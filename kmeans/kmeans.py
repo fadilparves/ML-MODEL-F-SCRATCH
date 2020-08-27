8d3267f243a4ee9a73073eb5bf3e1948ed32e3e0
fadilparves <fadil.parves@gmail.com>
2020-08-27 18:11:17 +0800

    Calculate centroid method added

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -2,3 +2,13 @@ class KMeans:
     def __init__(self, n_clusters, min_diff=1):
         self.n_clusters = n_clusters
         self.min_diff = min_diff
+
+    def calculate_center(self, points):
+        n_dim = len(points[0].coordinates)
+        vals = [0.0 for i in range(n_dim)]
+        for p in points:
+            for i in range(n_dim):
+                vals[i] += p.coordinates[i]
+                      
+        coords = [(v / len(points)) for v in vals]
+        return Point(coords)
